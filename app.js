be3aa9a169125dc8565fb8dc70e074c10c4945e1
abhishek.md <abhishek-pt3320@abhishek-pt3320.csez.zohocorpin.com>
2020-01-14 09:21:29 +0530

    db config implemented

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const url = require("./config/setup").mongoURL;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//bodyparser middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+
+//conecting to db
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch(err => console.log("Error : " + err));
 
 app.get("/", (req, res) => {
-  res.send("Hello!!");
+  // show sensor data
+  res.render("index");
+});
+
+app.post("/send", (req, res) => {
+  // add data to cloud
 });
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
---
+++ config/setup.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURL: "mongodb+srv://magesh:magesh123@cluster0-zb9mn.mongodb.net/test?retryWrites=true&w=majority"
+};
